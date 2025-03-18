f97eabfaa9dc23ece4012aa67cacd0cde18eb4d4
Rifky Faris <78222881+RifkyFaris@users.noreply.github.com>
2025-03-18 15:46:41 +0530

    Add files via upload

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,39 @@
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import {HelmetProvider} from 'react-helmet-async'
+import Home from './Market/Home'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <Router>
+      <HelmetProvider>
+        
+        <Routes>
+          {/* CarRepair routes */}
+        </Routes>
+
+        <Routes>
+          {/* CarWash routes */}
+        </Routes>
+
+        <Routes>
+          {/* Market routes */}
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+
+        <Routes>
+          {/* Profile routes */}
+        </Routes>
+
+        <Routes>
+          {/* Towing routes */}
+        </Routes>
+
+      </HelmetProvider>
+    </Router>
+  )
+}
+
+export default App
