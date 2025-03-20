fb29d69a595b6b1a312375a7a58864c81a3a9299
Rifky Faris <78222881+RifkyFaris@users.noreply.github.com>
2025-03-20 16:44:46 +0530

    Delete src directory

--- src/store.jsx
+++
@@ -1,9 +0,0 @@
-import {configureStore,combineReducers} from '@reduxjs/toolkit';
-import authReducer from './Profile/slice/userSlice'
-const reducer=combineReducers({
-    authState:authReducer,
-})
-const store=configureStore({
-    reducer,
-})
-export default store;
